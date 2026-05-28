80d4cd51e89d1dbb359128dd8d805e6f66924b27
Maria Jose Muñoz Matta <maria.munoz0507@gmail.com>
2026-05-28 18:02:25 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1287,7 +1287,6 @@ function App() {
         <a href="https://github.com/mariamunoz0507/informe_munmar" target="_blank" rel="noreferrer" style={{color:"#0ea5e9", fontSize:"12px", fontWeight:"600", textDecoration:"none"}}>📂 Ver Repositorio en GitHub →</a>
       </footer>
     </div>
-    </div>
   )
 }
 
